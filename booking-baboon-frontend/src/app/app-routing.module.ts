0cdd8ee5c2acdf6235c8ace81a42b3021dbeb9b5
maric300 <branko.maric300@gmail.com>
2023-11-25 18:18:25 +0100

    feat: added account page with edit button to each input field

--- booking-baboon-frontend/src/app/app-routing.module.ts
+++ booking-baboon-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./layout/home/home.component";
 import {LoginComponent} from "./layout/login/login.component";
 import {RegisterComponent} from "./layout/register/register.component";
+import {AccountComponent} from "./layout/account/account.component";
 
 const routes: Routes = [
   {component: HomeComponent, path:"home"},
   {component: LoginComponent, path:"login"},
-  {component: RegisterComponent, path:"register"}
+  {component: RegisterComponent, path:"register"},
+  {component: AccountComponent, path:"account"}
 ];
 
 @NgModule({
