8b70ebc94f14d65926e9c796e2994987ede1a8a1
maric300 <branko.maric300@gmail.com>
2023-11-30 01:58:52 +0100

    feat: connected accommodation card to backend

---
+++ booking-baboon-frontend/src/app/layout/Reviews/model/review.model.ts
@@ -0,0 +1,11 @@
+import {User} from "../../authentication/models/user.model";
+
+
+export interface Review {
+  id?: number;
+  reviewer?: User;
+  createdOn?: Date;
+  rating?: number;
+  comment?: string;
+
+}
