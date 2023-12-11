88109195c4a18407460897abbb2e8292e66a3de7
Ilija Bešlin <44114597+GearFifth@users.noreply.github.com>
2023-12-11 13:54:51 +0100

    feat: added date highlight

--- booking-baboon-frontend/src/app/layout/Reservations/reservation-request/reservation-request.component.ts
+++ booking-baboon-frontend/src/app/layout/Reservations/reservation-request/reservation-request.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AccommodationService } from "../../../services/accommodation/accommodation.service";
 import { Accommodation } from "../../accommodations/model/accommodation.model";
+import {AvailablePeriod} from "../../accommodations/model/available-period.model";
+import {MatCalendarCellClassFunction} from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-reservation-request',
@@ -10,6 +12,7 @@ import { Accommodation } from "../../accommodations/model/accommodation.model";
 })
 export class ReservationRequestComponent implements OnInit {
   @Input() accommodation!: Accommodation;
+  availablePeriods: AvailablePeriod[] = [];
 
   constructor(private fb: FormBuilder, private accommodationService: AccommodationService) {}
 
@@ -17,7 +20,6 @@ export class ReservationRequestComponent implements OnInit {
   price: string = '0';
 
   onSubmitClick() {
-
   }
 
   calculatePrice() {
@@ -29,13 +31,15 @@ export class ReservationRequestComponent implements OnInit {
     if (accommodationId && checkin && checkout) {
       this.accommodationService.calculateTotalPrice(accommodationId, checkin, checkout).subscribe({
         next: (totalPrice: number) => {
-          if(totalPrice !== 0 && guestNum !== 0){
+          if (totalPrice !== 0 && guestNum !== 0) {
             this.price = (guestNum * totalPrice).toString();
           } else {
-            this.price = "0"
+            this.price = "0";
           }
         },
-        error: (_) => { console.log("Error!"); }
+        error: (_) => {
+          console.log("Error!");
+        }
       });
     }
   }
@@ -51,9 +55,30 @@ export class ReservationRequestComponent implements OnInit {
     this.requestForm.valueChanges.subscribe(() => {
       this.calculatePrice();
     });
+
+    this.availablePeriods = this.accommodation.availablePeriods || [];
   }
 
   private getDateISOString(date: Date | null | undefined): string | null {
     return date ? date.toISOString().split('T')[0] : null;
   }
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    if (view === 'month') {
+      const currentDate = cellDate;
+
+
+      const isDateInAvailablePeriod = this.availablePeriods.some((period: AvailablePeriod) => {
+        const startTime = period.timeSlot?.startDate ? new Date(period.timeSlot.startDate) : null;
+        const endTime = period.timeSlot?.endDate ? new Date(period.timeSlot.endDate) : null;
+
+        return startTime && endTime && currentDate >= startTime && currentDate <= endTime;
+      });
+
+      return isDateInAvailablePeriod ? 'example-custom-date-class' : '';
+    }
+
+    return '';
+  };
+
 }
