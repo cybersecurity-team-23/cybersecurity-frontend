774b178f43472808dc818cf0d4a0fea1dbd7f0c2
markomitos <markomitosevic9@gmail.com>
2023-12-14 18:54:29 +0100

    fix: moved initialization to OnInit

--- booking-baboon-frontend/src/app/layout/accommodations/components/host-accommodation-card/host-accommodation-card.component.ts
+++ booking-baboon-frontend/src/app/layout/accommodations/components/host-accommodation-card/host-accommodation-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Accommodation} from "../../model/accommodation.model";
 import {ActivatedRoute} from "@angular/router";
 import {AccommodationReviewService} from "../../../../services/review/accommodation-review.service";
@@ -8,19 +8,20 @@ import {AccommodationReviewService} from "../../../../services/review/accommodat
   templateUrl: './host-accommodation-card.component.html',
   styleUrls: ['./host-accommodation-card.component.css']
 })
-export class HostAccommodationCardComponent {
-  @Input()
-  accommodation!: Accommodation;
+export class HostAccommodationCardComponent implements OnInit{
+
+  @Input() accommodation!: Accommodation;
 
   accommodationLocation: string | undefined;
   rating: number | undefined;
   ratingDisplay: string | undefined;
   constructor(private route: ActivatedRoute, private accommodationReviewService: AccommodationReviewService) {
-    if (this.accommodation !== undefined) {
+  }
+
+  ngOnInit(): void {
+    if (this.accommodation) {
       this.route.params.subscribe((params) => {
-
         this.accommodationLocation = this.accommodation.location?.address + ", " + this.accommodation.location?.city + ", " + this.accommodation.location?.country;
-
         this.accommodationReviewService.getAverageRatingFromAccommodation(this.accommodation.id!).subscribe({
           next: (rating: number) => { this.rating = rating;
             if (isNaN(this.rating ?? NaN)) {
