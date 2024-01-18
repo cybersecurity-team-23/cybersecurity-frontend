561ff121a5507c2dd6f48c56d97f022498433919
Ilija <ilija.beslin@gmail.com>
2024-01-18 13:27:36 +0100

    feat: added tests for whole reservation service

--- booking-baboon-frontend/src/app/layout/reservations/mocks/reservation.service.mock.ts
+++ booking-baboon-frontend/src/app/layout/reservations/mocks/reservation.service.mock.ts
@@ -16,5 +16,56 @@ const mockReservationRequest1: ReservationRequest = {
   price: 200
 };
 
+const mockReservation1: Reservation = {
+  id: 1,
+  accommodation: {
+    id: 1
+  },
+  timeSlot: {
+    startDate: '2024-04-15',
+    endDate: '2024-04-20'
+  },
+  guest: {
+    id: 5
+  },
+  status: ReservationStatus.Pending,
+  price: 200
+};
 
-export { mockReservationRequest1};
+const mockReservation2: Reservation = {
+  id: 2,
+  accommodation: {
+    id: 1
+  },
+  timeSlot: {
+    startDate: '2024-05-18',
+    endDate: '2024-05-22'
+  },
+  guest: {
+    id: 5
+  },
+  status: ReservationStatus.Approved,
+  price: 300
+};
+
+const mockReservation3: Reservation = {
+  id: 3,
+  accommodation: {
+    id: 1
+  },
+  timeSlot: {
+    startDate: '2024-01-05',
+    endDate: '2024-01-15'
+  },
+  guest: {
+    id: 5
+  },
+  status: ReservationStatus.Finished,
+  price: 800
+};
+
+const mockCancellationCount5 = 4;
+
+const mockReservationArray: Reservation[] = [mockReservation1, mockReservation2, mockReservation3];
+
+export { mockReservationRequest1, mockReservation1, mockReservation2, mockReservation3, mockReservationArray, mockCancellationCount5};
